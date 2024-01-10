1b5ef187d70f8df58ec1369f352a771efdc24596
bh1212 <156223526+bh1212@users.noreply.github.com>
2024-01-10 18:37:33 +0200

    Update riskapp2.py

--- riskapp2.py
+++ riskapp2.py
@@ -10,7 +10,7 @@ with col3:
     st.text('')
     st.image("/Users/aminghobar/Desktop/RADAMS.png") 
 with col4:
-    st.image("/Users/aminghobar/Desktop/John H Logo .jpeg")
+    st.image("John H Logo .jpeg")
 
 st.header('')
 
